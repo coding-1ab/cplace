1777657e0dabadec1c04b8c71c636a3ebe434e74
DocRAID <l01062506145@gmail.com>
2026-01-18 23:16:19 +0900

    [FUNC]pixel to generate png

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,32 +1,94 @@
-use rocket::{get, post};
-use rocket::serde::json::Json;
+mod image;
 
-#[derive(serde::Deserialize)]
+use crate::image::generate_overlay_png;
+use memmap::Mmap;
+use rocket::futures::AsyncWriteExt;
+use rocket::log::private::info;
+use rocket::serde::json::Json;
+use rocket::{get, post};
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+
+#[derive(serde::Deserialize, serde::Serialize)]
 pub struct Pixel {
     pub r: u8,
     pub g: u8,
     pub b: u8,
+    pub a: u8,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 pub struct Condinate {
     pub x: f32,
     pub y: f32,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct PixelData {
     pub pixel: Pixel,
     pub condinate: Condinate,
 }
 
-#[post("/draw",format = "json", data = "<pixel_data>")]
+#[post("/draw", format = "json", data = "<pixel_data>")]
 pub fn draw(pixel_data: Json<PixelData>) -> Result<(), ()> {
     todo!();
 }
 
-#[get("/drawing/<chunkx>/<chunky>")]
-pub fn get_drawing(chunkx: i32, chunky: i32) -> Result<Json<Vec<PixelData>>, ()> {
-    todo!();
+#[get("/drawing/<chunkx>/<chunky>/<zoom_lv>")]
+pub fn get_drawing(chunkx: i32, chunky: i32, zoom_lv: u8) -> Result<Json<Vec<PixelData>>, ()> {
+    //
+
+    //example
+    Ok(Json(vec![PixelData {
+        pixel: Pixel {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 0,
+        },
+        condinate: Condinate { x: 0.0, y: 0.0 },
+    }]))
+}
+
+
+
+//test and usage guide function
+#[get("/px_data_to_png_test")]
+pub fn px_data_to_png_test() {
+    info!("px_data_to_png_test");
+    let sample_data = vec![
+        PixelData {
+            pixel: Pixel {
+                r: 255,
+                g: 0,
+                b: 0,
+                a: 255,
+            },
+            condinate: Condinate { x: 10.0, y: 10.0 },
+        },
+        PixelData {
+            pixel: Pixel {
+                r: 0,
+                g: 255,
+                b: 0,
+                a: 255,
+            },
+            condinate: Condinate { x: 20.0, y: 20.0 },
+        },
+        PixelData {
+            pixel: Pixel {
+                r: 0,
+                g: 0,
+                b: 255,
+                a: 255,
+            },
+            condinate: Condinate { x: 30.0, y: 30.0 },
+        },
+    ];
+
+    let png_data: Vec<u8> = generate_overlay_png(&sample_data);
+    let mut file = File::create("test.png".to_string()).unwrap();
+    let _ = file.write_all(&png_data);
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,10 @@
-#[macro_use] extern crate rocket;
-use server::draw;
+#[macro_use]
+extern crate rocket;
+use server::{draw, px_data_to_png_test};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/post", routes![draw])
+    rocket::build()
+        .mount("/post", routes![draw])
+        .mount("/test", routes![px_data_to_png_test])
 }
