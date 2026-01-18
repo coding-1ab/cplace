419d2de1c726eb5753421f45861c7b542fad4f64
woals-nightly <10326bphs25@sonline20.sen.go.kr>
2026-01-18 23:34:55 +0900

    으악 컨플릭트

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,3 +1,4 @@
+use mmap_sync::synchronizer::{Synchronizer, SynchronizerError};
 use rocket::{get, post};
 use rocket::serde::json::Json;
 
@@ -10,8 +11,10 @@ pub struct Pixel {
 
 #[derive(serde::Deserialize)]
 pub struct Condinate {
-    pub x: f32,
-    pub y: f32,
+    pub chunk_x: i32,
+    pub chunk_y: i32,
+    pub x: i32,
+    pub y: i32,
 }
 
 #[derive(serde::Deserialize)]
@@ -21,12 +24,19 @@ pub struct PixelData {
     pub condinate: Condinate,
 }
 
+pub struct Chunk {
+    pub pixels: [[Pixel; 64]; 64],
+}
+
 #[post("/draw",format = "json", data = "<pixel_data>")]
-pub fn draw(pixel_data: Json<PixelData>) -> Result<(), ()> {
-    todo!();
+pub fn draw(pixel_data: Json<PixelData>) -> Result<(), SynchronizerError > {
+    let mut synchronizer = Synchronizer::new("/tmp/chunk_data".as_ref());
+    let data = unsafe { synchronizer.read::<Chunk>(true) }.expect("failed to read data");
+
+    Ok(())
 }
 
 #[get("/drawing/<chunkx>/<chunky>")]
-pub fn get_drawing(chunkx: i32, chunky: i32) -> Result<Json<Vec<PixelData>>, ()> {
-    todo!();
+pub fn get_drawing(chunkx: i32, chunky: i32) -> Result<Json<Chunk>>, SynchronizerError> {
+    
 }
